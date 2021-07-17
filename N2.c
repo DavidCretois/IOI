81ce281800238041be960e0139effcbd580061a3
David Cretois <42750559+DavidCretois@users.noreply.github.com>
2021-07-17 19:26:45 +0200

    ✅

---
+++ N2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "robot.h"
+ 
+int main()
+{
+   remplir(3);
+   transferer(3, 5);
+   remplir(3);
+   transferer(3, 5);
+   vider(5);
+   transferer(3, 5);
+   remplir(3);
+   transferer(3, 5);
+}
